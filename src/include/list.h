1f742bf0a808873c6df94414ca1160710f625525
Ty Seawood <tyler@tsservices.pro>
2023-04-10 14:52:12 -0700

    initial commit

---
+++ src/include/list.h
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class List
+{
+   
+    public:
+    List(){
+        //constructor
+    }
+    ~List(){
+        //destructor
+    }
+    vector<string> list;
+    string name; 
+    
+    void print_menu();
+    void print_list();
+    void add_item();
+    void delete_item(); 
+    
+    private:
+};
+
